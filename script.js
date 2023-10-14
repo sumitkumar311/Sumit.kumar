98e72d92c65399f9550dd8afba23615dbbed2682
Sumit Kumar <117653927+sumitkumar311@users.noreply.github.com>
2023-10-14 15:00:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,51 @@ ScrollReveal({
  ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'top' });
  ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+ 
+ function MouseMover(){
+    window.addEventListener("mousemove", function(dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+MouseMover();
+
+function circleChaptaKaro() {
+    // define default scale value
+    var xscale = 1;
+    var yscale = 1;
+  
+    var xprev = 0;
+    var yprev = 0;
+  
+    window.addEventListener("mousemove", function (dets) {
+      clearTimeout(timeout);
+  
+      xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+      yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+  
+      xprev = dets.clientX;
+      yprev = dets.clientY;
+  
+      circleMouseFollower(xscale, yscale);
+  
+      timeout = setTimeout(function () {
+        document.querySelector(
+          "#minicircle"
+        ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+      }, 100);
+    });
+  }
+  
+  function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    });
+  }
+  
+  circleChaptaKaro();
+  circleMouseFollower();
+  
+  
